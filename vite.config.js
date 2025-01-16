e413de08776b9c80a126a814c6bd81472aecccb7
APbrothers2110 <atharvphakatkar@gmail.com>
2025-01-16 14:07:47 +0530

    dist folder added

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'; 
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
+  plugins:[react()],
+  base:"/reliabilitygmrt/",
+
   server: {
     proxy: {
       '/upload': {
